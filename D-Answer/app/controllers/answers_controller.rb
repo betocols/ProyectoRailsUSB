0c07c056a254de6199de4bcad65ae408921e381d
Alberto Cola <betocolsf@gmail.com>
2013-06-13 13:39:07 -0430

    Vista show pregunta

--- D-Answer/app/controllers/answers_controller.rb
+++ D-Answer/app/controllers/answers_controller.rb
@@ -48,7 +48,8 @@ class AnswersController < ApplicationController
     @answer = @question.answers.new(params[:answer])
     @answer.user = current_user
     @answer.score = 0
-    
+    @user = User.find(@question.user_id)
+
     respond_to do |format|
       if @answer.save
         UserMailer.answer_email(@user, @question).deliver
