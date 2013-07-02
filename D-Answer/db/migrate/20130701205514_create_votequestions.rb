a5f3007b829cadea8d332d685e565adfe89067ef
Alberto Cola <betocolsf@gmail.com>
2013-07-01 23:59:09 -0430

    sin drop

--- D-Answer/db/migrate/20130701205514_create_votequestions.rb
+++ D-Answer/db/migrate/20130701205514_create_votequestions.rb
@@ -1,6 +1,5 @@
 class CreateVotequestions < ActiveRecord::Migration
   def change
-    drop_table :votequestions
     create_table :votequestions do |t|
       t.integer :user_id
       t.integer :question_id
